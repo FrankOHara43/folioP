91128f9e23f5e088337bab28c874af3ed0cb651c
Subham <rajshubham199@gmail.com>
2024-04-04 23:21:33 +0530

    fixed that dot and added bootstrap

--- src/experince.js
+++ src/experince.js
@@ -1,35 +1,54 @@
 import React from "react";
+import "./Experience.css";
 
 function Experience() {
-    return (
-      <div className="experience">
-        <div className="experience-content">
-            <div className="expList">Experience</div>
-            <div className="seperator">
-             <div className="line"></div>
-             <div className="dot"></div>
-            </div>
-            <div className="exp">Experience1</div>
-            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
-            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
-            <div className="seperator">
-             <div className="line"></div>
-             <div className="dot"></div>
-            </div>
-            <div className="exp">Experience2</div>
-            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
-            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
-            <div className="seperator">
-             <div className="line"></div>
-             <div className="dot"></div>
-            </div>
-            <div className="exp">Experience3</div>
-            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
-            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+  return (
+    // <div className="experience">
+    //   <div className="experience-content">
+    //     <div className="expList">Experience</div>
+    //     <div className="seperator">
+    //       <div className="line"></div>
+    //       <div className="dot"></div>
+    //     </div>
+    //     <div className="exp">Experience1</div>
+    //     <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+    //     <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+    //     <div className="seperator">
+    //       <div className="line"></div>
+    //       <div className="dot"></div>
+    //     </div>
+    //     <div className="exp">Experience2</div>
+    //     <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+    //     <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+    //     <div className="seperator">
+    //       <div className="line"></div>
+    //       <div className="dot"></div>
+    //     </div>
+    //     <div className="exp">Experience3</div>
+    //     <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+    //     <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+    //   </div>
+    //   <div className="">
+
+    //   </div>
+    // </div>
+    <>
+      <div className="container ">
+        <div className="expList">Experience</div>
+        <div className="flex ">
+          <div className="dot1"></div>
+          <div>
+            <div className="exp-title">Experience3</div>
+            <div className="exp-timeline">Apr 2023 - present . 1 yr 1 mos</div>
+            <div className="exp-address">Navi Mumbai,Maharashtra,India</div>
+          </div>
+
         </div>
       </div>
-    );
-  }
-  
-  export default Experience;
-  
+
+    </>
+  );
+}
+
+export default Experience;
+
