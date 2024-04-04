35e5212f94ad720041b0dd54b8bd45fde0304ceb
FrankOHara43 <111985913+FrankOHara43@users.noreply.github.com>
2024-04-04 20:53:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Header from './header';
+import Experience from './experince';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Experience/>
+
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/experince.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Experience() {
+    return (
+      <div className="experience">
+        <div className="experience-content">
+            <div className="expList">Experience</div>
+            <div className="seperator">
+             <div className="line"></div>
+             <div className="dot"></div>
+            </div>
+            <div className="exp">Experience1</div>
+            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+            <div className="seperator">
+             <div className="line"></div>
+             <div className="dot"></div>
+            </div>
+            <div className="exp">Experience2</div>
+            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+            <div className="seperator">
+             <div className="line"></div>
+             <div className="dot"></div>
+            </div>
+            <div className="exp">Experience3</div>
+            <div className="timeline">Apr 2023 - present . 1 yr 1 mos</div>
+            <div className="subtext">Navi Mumbai,Maharashtra,India</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Experience;
+  
